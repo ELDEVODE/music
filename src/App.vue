8201a374efd2eeb9a614a6dca0a8c6f4b5797f0b
El-Praise Ayo <elpraise20@gmail.com>
2024-03-06 20:18:44 +0100

    form validations

--- src/App.vue
+++ src/App.vue
@@ -190,7 +190,6 @@
             <a href="#" class="font-bold block text-gray-600">Song Title</a>
             <span class="text-gray-500 text-sm">Artist Name</span>
           </div>
-
           <div class="text-gray-600 text-lg">
             <span class="comments">
               <i class="fa fa-comments text-gray-600"></i>
@@ -268,11 +267,11 @@
 </template>
 
 <script>
-import Auth from "./components/Auth.vue";
-import AppHeader from "./components/Header.vue";
+import Auth from './components/Auth.vue';
+import AppHeader from './components/Header.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     AppHeader,
     Auth,
